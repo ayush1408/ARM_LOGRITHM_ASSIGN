4318a957fcbfb07fbad4fe37f220297e6bfa9bc3
ayush1408 <60623980+ayush1408@users.noreply.github.com>
2020-10-23 01:57:40 -0700

    Update ARM_LOG_ASSIGNMENT.s

--- ARM_LOG_ASSIGNMENT.s
+++ ARM_LOG_ASSIGNMENT.s
@@ -1,33 +1,33 @@
      area     appcode, CODE, READONLY
      export __main
 	 ENTRY 
-__main  function		 
-              VLDR.f32 S3, = 1.0;  A fixed register to use value 1 anywhere
-       VLDR.f32 S5, =1;
+__main  function		
+              VLDR.f32 S0, = 1.0;  A fixed register to use value 1 anywhere
+       VLDR.f32 S1, =1;
 	   VLDR.f32 S11, =0;	     A fixed register to use value 0 anywhere
-       VLDR.f32 S10,= 0 ; S10 ---> RESULT
-       VLDR.f32 S12,= 1;  S12----> x 
-       VLDR.f32 S2,= 0; S2----> SIGN
-       VLDR.f32  S9,= 1 ;  S9--->COUNT
+       VLDR.f32 S5,= 0 ; S5 ---> RESULT
+       VLDR.f32 S12,= 0.3;  S12----> x 
+       VLDR.f32 S2,= 0; S2---->A FLAG USED FOR SIGN
+       VLDR.f32  S3,= 1 ;  S3--->COUNT
 	   
-T1 		VMOV.f32 S9,S5 ; adding value of S5(which is incremened in last step to S9.
-		VMOV.f32 S8,S12 ; giving value of x(S12) to s8  
-		VCMP.f32 S9,S3 ;
-		VMRS APSR_nzcv,FPSCR
-    ITTE GT
-		
-		VMULGT.F32 S8,S12,S8; multiplying x. //then part//
-		VMOVGT.F32 S9,S3;
-        
-		VDIVLE.F32 S8,S8,S5;     //else part//
+T1 		VMOV.f32 S3,S1 ; adding value of S1(which is incremened in last step to S3.
+		VMOV.f32 S4,S12 ; giving value of x(S12) to s4
+T3		VCMP.f32 S3,S0 ;   //if value of count LE to 1, branch to division and sign block(T2)// 
+		VMRS APSR_nzcv,FPSCR ; //for updating flags//
+    
+		BLE T2         ;   //Then part//
+		VMULGT.F32 S4,S12,S4; multiplying x
+		VSUBGT.F32 S3,S3,S0;
+        BGT T3
+T2		VDIVLE.F32 S4,S4,S1;     //else part//
 		VCMPLE.F32 S2,#0.0 ; 
 		VMRS APSR_nzcv,FPSCR
-		ITTE EQ;
-		VADDEQ.f32 S10,S8,S10;    //to get +ve sign(Addition of terms)//
-		VMOVEQ.f32 S2,S3;         
-		VSUBNE.F32 S10,S8,S10;    //to get -ve sign(Substraction of terms)//
-		VMOVNE.f32 S2,S10;        
-		VADD.F32 S5,S5,S3;
+		VADDEQ.f32 S5,S4,S5;    //to get +ve sign(Addition of terms)//
+		VMOVEQ.f32 S2,S0;         //Update flag//
+		VSUBNE.F32 S5,S5,S4;    //to get -ve sign(Substraction of terms)//
+		VMOVNE.f32 S2,S11;        //Update flag//
+		VADD.F32 S1,S1,S0;        // increasing count value for furthur terms//
 		B T1 ;
 	endfunc
 	end
+;If u GIVE ln(1+0.3) and get correct result that is 0.262. 
